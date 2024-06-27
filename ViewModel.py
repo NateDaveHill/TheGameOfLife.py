062af1da0b034a2dfd3f5ddcdb6a02e6a80943ec
NateDaveHill <nathanhill123@gmail.com>
2024-06-27 13:40:14 -0400

    view model working

--- ViewModel.py
+++ ViewModel.py
@@ -1,10 +1,34 @@
 from tkinter import *
+import DataModel
 
-root = Tk()
-root.title('The Game Of Life')
+cell_size = 5
 
-start_button = Button(root, text='Start', width=12)
+def setup():
+    global root, grid_view, cell_size, start_button, clear_button, choice
 
-start_button.pack()
+    root = Tk()
+    root.title('The Game Of Life')
 
-mainloop()
+    grid_view = Canvas(root, width=DataModel.width*cell_size,
+                     height=DataModel.height*cell_size,
+                     borderwidth=0,
+                     highlightthickness=0,
+                     bg='white')
+    
+    start_button = Button(root, text='Start', width=12)
+    clear_button = Button(root, text='Clear', width=12)
+
+    choice = StringVar(root)
+    choice.set('Choose a Pattern')
+    option = OptionMenu(root, choice, 'Choose a Pattern', 'glider', 'glider gun', 'random')
+    option.config(width=20)
+
+    grid_view.pack()
+    start_button.pack()
+    option.pack()
+    clear_button.pack()
+
+
+if __name__ == '__main__':
+    setup()
+    mainloop()
